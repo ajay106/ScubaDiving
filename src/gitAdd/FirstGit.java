a93f4e5181b5e28840f303dd22fa9bdb0d5676bc
ajay106 <124262434+ajay106@users.noreply.github.com>
2023-02-12 18:49:14 +0530

    Update FirstGit.java

--- src/gitAdd/FirstGit.java
+++ src/gitAdd/FirstGit.java
@@ -5,7 +5,8 @@ public class FirstGit {
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
-		System.out.println(a+b);
+		int c=5;
+		System.out.println(a+b*c);
 		System.out.println("Welcome to git" );
 
 	}
