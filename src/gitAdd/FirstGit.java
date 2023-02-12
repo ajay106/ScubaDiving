77ddd39e75724b609ca0f12075875714776c0579
Ajay Sambhaji Patil <ajays@maveric-systems.com>
2023-02-12 20:00:04 +0530

    Adding one more variable

--- src/gitAdd/FirstGit.java
+++ src/gitAdd/FirstGit.java
@@ -6,7 +6,8 @@ public class FirstGit {
 		int a=10;
 		int b=20;
 		int c=5;
-		System.out.println(a+b*c);
+		int d=10;
+		System.out.println(a+b*c+d);
 		System.out.println("Welcome to git" );
 
 	}
