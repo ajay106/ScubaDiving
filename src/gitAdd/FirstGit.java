352cc32531dde638b0cb7b7d950f73832ca34029
Ajay Sambhaji Patil <ajays@maveric-systems.com>
2023-02-12 18:46:09 +0530

    third commit

--- src/gitAdd/FirstGit.java
+++ src/gitAdd/FirstGit.java
@@ -3,7 +3,10 @@ package gitAdd;
 public class FirstGit {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to git");
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		System.out.println("Welcome to git" );
 
 	}
 
